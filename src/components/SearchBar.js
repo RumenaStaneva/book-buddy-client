24f6dd454a746b1a19c744f37b0d2d29b4d2981c
Rumena Staneva <rumenastaneva2106@gmail.com>
2023-07-23 17:43:58 +0300

    added functionality to search page when inside 404

---
+++ src/components/SearchBar.js
@@ -0,0 +1,61 @@
+// SearchBar.js - Search bar component
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import pages from '../data';
+import '../styles/SearchBar.css'
+
+const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const [showError, setShowError] = useState(false);
+
+    const handleSearch = (event) => {
+        const term = event.target.value;
+        setSearchTerm(term);
+
+        if (term.trim() === '') {
+            setShowError(true);
+            setSearchResults([]);
+        } else {
+            setShowError(false);
+            const filteredResults = pages.filter((page) => {
+                const titleMatch = page.title && page.title.toLowerCase().includes(term.toLowerCase());
+                return titleMatch;
+            });
+
+            setSearchResults(filteredResults);
+        }
+    };
+
+    return (
+        <div className="search-bar">
+            <input
+                type="text"
+                value={searchTerm}
+                onChange={handleSearch}
+                placeholder="Search..."
+                className="search-bar__input"
+            />
+
+            {searchResults.length > 0 ? (
+                <ul className="search-bar__results-list">
+                    {searchResults.map((page) => (
+                        <li key={page.id} className="search-bar__result-item">
+                            <NavLink to={`${page.link}`} className="search-bar__result-title">
+                                {page.title}
+                            </NavLink>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                searchTerm.trim() !== '' && <p>No results found.</p>
+            )}
+
+            {showError ?
+                <p className="search-bar__error-message">Plese enter valid input</p>
+                : null}
+        </div>
+    );
+};
+
+export default SearchBar;
---
+++ src/data.js
@@ -0,0 +1,30 @@
+// TODO: Find better approach
+const pages = [
+    {
+        id: 1,
+        title: 'Home',
+        link: '/'
+    },
+    {
+        id: 2,
+        title: 'About',
+        link: '/about'
+    },
+    {
+        id: 3,
+        title: 'Login',
+        link: '/login'
+    },
+    {
+        id: 4,
+        title: 'Sign Up',
+        link: '/sign-up'
+    },
+    {
+        id: 5,
+        title: 'Time Swap',
+        link: '/time-swap'
+    },
+];
+
+export default pages;
