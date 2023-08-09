2b1bb49f10583c321115bdf452811995105ae0f8
Rumena Staneva <rumenastaneva2106@gmail.com>
2023-08-09 13:23:44 +0300

    fixed typo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Home from './pages/Home';
 import About from './pages/About';
 import TimeSwap from './pages/TimeSwap';
 import Login from './pages/Login';
-import SignUp from './pages/Signup';
+import SignUp from './pages/SignUp';
 import PasswordReset from './pages/PasswordReset';
 import ErrorPage from './pages/ErrorPage';
 import './styles/App.css'
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useAuthContext } from './hooks/useAuthContext';
+import BookDetailsPage from './pages/BookDetailsPage';
 
 function App() {
 
@@ -23,6 +24,7 @@ function App() {
         <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
         <Route path="/sign-up" element={!user ? <SignUp /> : <Navigate to="/" />} />
         <Route path="/forgotten-password" element={<PasswordReset />} />
+        <Route path="/books/:id" element={<BookDetailsPage />} />
 
         <Route path='*' element={<ErrorPage />} />
       </Routes>
