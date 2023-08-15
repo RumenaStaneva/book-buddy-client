e766ca1c160481318a020e0366b2130aabcc5815
Rumena Staneva <rumenastaneva2106@gmail.com>
2023-08-16 00:02:00 +0300

    added some basic colors for each genre

--- src/pages/ListAllBooks.js
+++ src/pages/ListAllBooks.js
@@ -1,10 +1,14 @@
 import { useCallback, useEffect, useState } from "react";
 import Spinner from 'react-spinner-material';
-
-// import { useParams } from "react-router-dom";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import { useLocation } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
 import NavBar from "../components/NavBar";
+import { Modal } from "@mui/material";
+import Dropdown from "../components/Dropdown";
+import categoryColors from "../constants/categoryColors";
+import '../styles/ListAllBooks.css'
 
 function ListAllBooks() {
     const location = useLocation();
@@ -15,7 +19,24 @@ function ListAllBooks() {
     const [page, setPage] = useState(1);
     const [limit, setLimit] = useState(5);
     const [totalPages, setTotalPages] = useState(1);
+    const [currentBook, setCurrentBook] = useState();
+    const [open, setOpen] = useState(false);
+    const shelfOptions = ['Want to read', 'Currently reading', 'Read'];
+
+    const handleOpen = (book) => {
+        setCurrentBook(book)
+        setOpen(true);
+    }
+    const handleClose = () => setOpen(false);
+
     const { user } = useAuthContext();
+
+    const getShelfName = () => {
+        if (shelfNum === 0) return 'Want to read';
+        if (shelfNum === 1) return 'Currently reading';
+        if (shelfNum === 2) return 'Read';
+        return 'Unknown Shelf';
+    };
 
     const fetchBooks = useCallback(
         async () => {
@@ -30,6 +51,7 @@ function ListAllBooks() {
                 const data = await response.json();
                 setIsLoading(false);
                 setBooks(data.books);
+                // console.log(data.books);
                 setTotalPages(data.totalPages);
             } catch (error) {
                 console.error('Error fetching user data:', error);
@@ -45,6 +67,22 @@ function ListAllBooks() {
         }
     }, [user, fetchBooks]);
 
+    const handleMoveToShelf = () => {
+        //todo
+    }
+
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    };
+
     return <>
         <NavBar />
         {isLoading ? (
@@ -52,33 +90,70 @@ function ListAllBooks() {
                 <Spinner radius={120} color={"#E02D67"} stroke={5} visible={true} />
             </div>
         ) : (
-            <div>
-                <h1>All books on {shelfNum === 0
-                    ? 'Want to read'
-                    : shelfNum === 1
-                        ? 'Currently reading'
-                        : shelfNum === 2
-                            ? 'Read'
-                            : 'Unknown Shelf'}</h1>
-                {console.log(shelfNum)}
-                {books && books.map(book => {
-                    return (
-                        <div key={book._id}>
-                            <p>{book.title}</p>
-                        </div>
-                    )
-                })}
-                <div className="pagination">
-                    {page > 1 && (
-                        <button onClick={() => setPage(page - 1)}>Previous</button>
-                    )}
-                    <span>Page {page} of {totalPages}</span>
-                    {page < totalPages && (
-                        <button onClick={() => setPage(page + 1)}>Next</button>
-                    )}
-                </div>
-            </div>
-        )}
+            books.length > 0 ?
+                < main className="books__list-all">
+                    {currentBook ?
+                        //TODO STYLE MODAL AND MAKE CHANGE SHELF FUNCTIONALITY
+                        <Modal
+                            open={open}
+                            onClose={handleClose}
+                            aria-labelledby="modal-modal-title"
+                        >
+                            <Box sx={style}>
+                                <h3>Change shelf</h3>
+                                <Typography id="modal-modal-title" variant="h6" component="h2">
+                                    {currentBook.title}
+                                </Typography>
+                                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                    Currently on: {getShelfName()} shelf
+                                </Typography>
+                                <Dropdown options={Object.values(shelfOptions)} onSelect={handleMoveToShelf} />
+                            </Box>
+                        </Modal>
+                        : null}
+                    <div className="heading-section">
+                        <h1 className="section-title">All books on {getShelfName()}</h1>
+                    </div>
+                    <div className="books__container">
+                        {books && books.map(book => {
+                            const categoryColor = categoryColors[book.category] || '#FFFFFF';
+                            const bookStyle = {
+                                background: `linear-gradient(${categoryColor}, rgba(0, 0, 0, 0))`,
+                            };
+
+                            return (
+                                <div key={book._id} className="book" style={bookStyle}>
+                                    <img
+                                        src={
+                                            book.thumbnail === undefined
+                                                ? require('../images/image-not-available.png')
+                                                : `${book.thumbnail}`
+                                        } alt={`${book.title}`}
+                                        className='book__image'
+                                    />
+                                    <div className="book__info">
+                                        <h2 className="book__title">{book.title}</h2>
+                                        <p className="book__authors">{book.authors.map((author, index) => index === book.authors.length - 1 ? author : `${author}, `)}</p>
+                                        <p className="book__category" style={{ backgroundColor: categoryColor }}>{book.category}</p>
+                                        <button onClick={() => handleOpen(book)} className="book__btn">Move to different shelf</button>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className="pagination">
+                        {page > 1 && (
+                            <button onClick={() => setPage(page - 1)}>Previous</button>
+                        )}
+                        <span>Page {page} of {totalPages}</span>
+                        {page < totalPages && (
+                            <button onClick={() => setPage(page + 1)}>Next</button>
+                        )}
+                    </div>
+                </main >
+                : <h1>No books on shelf {getShelfName()}</h1>
+        )
+        }
     </>
 }
 
