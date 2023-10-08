8d2c8d5602de420dad84eb915c56eba57b851c57
Rumena Staneva <rumenastaneva2106@gmail.com>
2023-10-08 22:41:59 +0300

    added avatar editor

--- src/components/ProfilePicture.js
+++ src/components/ProfilePicture.js
@@ -1,81 +1,89 @@
-import React, { useEffect, useRef, useState, useCallback } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
+import AvatarEditor from 'react-avatar-editor';
 import { useAuthContext } from '../hooks/useAuthContext';
 import { useDispatch } from "react-redux";
 import { setError, clearError } from '../reducers/errorSlice';
 import Spinner from 'react-spinner-material';
+import '../styles/Profile.css'
 
-
-function ProfilePicture() {
+function ProfilePicture({ handleProfileClick }) {
     const uploadedImage = useRef(null);
     const imageUploader = useRef(null);
     const [imageUploaded, setImageUploaded] = useState(false);
     const [encodedImage, setEncodedImage] = useState('');
-    const [profileImage, setProfileImage] = useState('');
     const [isLoading, setIsLoading] = useState(true);
     const { user, dispatch } = useAuthContext();
     const dispatchError = useDispatch();
+    const [editor, setEditor] = useState(null);
+    const [scale, setScale] = useState(1);
 
     const handleImageUpload = async (e) => {
         const [file] = e.target.files;
         if (file) {
             const reader = new FileReader();
-            const { current } = uploadedImage;
-            setImageUploaded(true);
-            current.file = file;
             reader.onload = (e) => {
                 const imageDataUrl = reader.result;
-                // console.log(imageDataUrl);
                 setEncodedImage(imageDataUrl);
-                current.src = e.target.result;
+                if (uploadedImage.current) {
+                    uploadedImage.current.file = file;
+                    uploadedImage.current.src = imageDataUrl;
+                }
             }
             reader.readAsDataURL(file);
-
-
+            setImageUploaded(true);
         }
-
     }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         setIsLoading(true);
 
-        console.log(encodedImage);
-        const formData = new FormData();
-        formData.append('profilePicture', encodedImage);
-        try {
-            const response = await fetch(`${process.env.REACT_APP_LOCAL_HOST}/users/upload-profile-picture`, {
-                method: "POST",
-                headers: {
-                    Authorization: `Bearer ${user.token}`,
-                },
-                body: formData,
-            });
+        if (editor) {
+            const canvas = editor.getImageScaledToCanvas();
+            const dataUrl = canvas.toDataURL();
+            const formData = new FormData();
 
-            if (response.status === 401) {
-                dispatchError(setError({ message: 'Unauthorized access' }));
-                console.error('Unauthorized access');
-                return;
+            formData.append('profilePicture', dataUrl);
+
+            try {
+                const response = await fetch(`${process.env.REACT_APP_LOCAL_HOST}/users/upload-profile-picture`, {
+                    method: "POST",
+                    headers: {
+                        Authorization: `Bearer ${user.token}`,
+                    },
+                    body: formData,
+                });
+
+                if (response.status === 401) {
+                    dispatchError(setError({ message: 'Unauthorized access' }));
+                    console.error('Unauthorized access');
+                    return;
+                }
+
+                const responseData = await response.json();
+                if (response.ok) {
+                    const localstorageUser = JSON.parse(localStorage.getItem('user'));
+                    localstorageUser.profilePicture = responseData.user.profilePicture;
+                    localStorage.setItem('user', JSON.stringify(localstorageUser));
+                    dispatch({ type: 'LOGIN', payload: localstorageUser });
+                    dispatchError(clearError());
+                    setEncodedImage('');
+                    handleProfileClick();
+                    setScale(1);
+                    setEditor(null);
+                } else {
+                    console.error('Error uploading profile picture:', responseData.error);
+                    dispatchError(setError({ message: `Error uploading profile picture: ${responseData.error}` }));
+                }
+            } catch (error) {
+                console.error('Network error occurred:', error);
+                dispatchError(setError({ message: `Network error occurred: ${error.message}` }));
             }
-
-            const responseData = await response.json();
-            console.log(responseData);
-            if (response.ok) {
-                const localstorageUser = JSON.parse(localStorage.getItem('user'));
-                localstorageUser.profilePicture = responseData.user.profilePicture;
-                localStorage.setItem('user', JSON.stringify(localstorageUser));
-                dispatch({ type: 'LOGIN', payload: localstorageUser });
-                dispatchError(clearError());
-
-            } else {
-                // Handle error scenarios
-                console.error('Error uploading profile picture:', responseData.error);
-                dispatchError(setError({ message: `Error uploading profile picture: ${responseData.error}` }));
-            }
-        } catch (error) {
-            console.error('Network error occurred:', error);
-            dispatchError(setError({ message: `Network error occurred: ${error.message}` }));
+            setIsLoading(false);
+        } else {
+            console.error('Editor instance is null');
+            setIsLoading(false);
         }
-        setIsLoading(false);
     }
 
     const fetchUserData = useCallback(async () => {
@@ -93,8 +101,7 @@ function ProfilePicture() {
                 return;
             }
 
-            const data = await response.json();
-            setProfileImage(data.userProfile.profilePicture)
+            await response.json();
             setIsLoading(false);
             dispatchError(clearError());
         } catch (error) {
@@ -117,16 +124,9 @@ function ProfilePicture() {
                     <Spinner radius={120} color={"#E02D67"} stroke={5} visible={true} />
                 </div>
             ) : (
-                <form
-                    style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                        justifyContent: "center"
-                    }}
-                >
-                    <div style={{ position: 'relative', width: '100%' }}>
-                        <label htmlFor="profile-picture-uploader" style={{ position: 'absolute', left: 0, right: 0, display: imageUploaded ? 'none' : '' }}>Click to add your image</label>
+                <form className="profile-form">
+                    <div className="image-uploader">
+                        <label htmlFor="profile-picture-uploader" className={`upload-label ${imageUploaded ? 'hidden' : ''}`}>Click to add your image</label>
                         <input
                             type="file"
                             name="profile-picture-uploader"
@@ -138,26 +138,40 @@ function ProfilePicture() {
                             }}
                         />
                     </div>
-                    <div
-                        style={{
-                            height: "400px",
-                            width: "400px",
-                            border: "2px dashed black"
-                        }}
-                        onClick={() => imageUploader.current.click()}
-                    >
+                    {!encodedImage.length > 0 ?
+                        <div className="image-container" onClick={() => imageUploader.current.click()}>
 
-                        <img
-                            ref={uploadedImage}
-                            style={{
-                                width: "100%",
-                                height: "100%",
-                            }}
-                        />
-                    </div>
-                    <button type="submit" onClick={handleSubmit}>Add picture</button>
+                        </div>
+                        :
+                        encodedImage.length > 0 && (
+                            <div className="editor-container">
+                                <AvatarEditor
+                                    ref={(editor) => setEditor(editor)}
+                                    image={encodedImage}
+                                    width={250}
+                                    height={250}
+                                    border={50}
+                                    borderRadius={500}
+                                    color={[255, 255, 255, 0.6]}
+                                    scale={scale}
+                                />
+                                <div className="zoom-control">
+                                    <label>Zoom:</label>
+                                    <input
+                                        type="range"
+                                        min="1"
+                                        max="2"
+                                        step="0.01"
+                                        value={scale}
+                                        onChange={(e) => setScale(parseFloat(e.target.value))}
+                                    />
+                                </div>
+                                <button className="update__button" onClick={handleSubmit}>Add picture</button>
+                            </div>
+                        )}
                 </form >
-            )}
+            )
+            }
         </>
     )
 }
