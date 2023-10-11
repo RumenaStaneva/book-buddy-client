023f40560776687ccfa661844ee6f613a0681eed
Rumena Staneva <rumenastaneva2106@gmail.com>
2023-10-11 14:44:33 +0300

    conditional rendering on add screen time button and user reading time table

--- src/pages/TimeSwap.js
+++ src/pages/TimeSwap.js
@@ -1,14 +1,19 @@
 import NavBar from "../components/NavBar";
 import Countdown from "../components/Countdown";
 import Button from "../components/Button";
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
+import { useAuthContext } from "../hooks/useAuthContext";
 import Calendar from "../components/Calendar";
 import ReadingTimeTable from "../components/ReadingTimeTable";
-
+import AddScreenTimeModal from "../components/AddScreenTimeModal";
 const TimeSwap = () => {
+    const [isOpenAddScreenTime, setIsOpenAddScreenTime] = useState(false);
+    const [hasAlreadyAddedScreenTime, setHasAlreadyAddedScreenTime] = useState(false);
+    const [readingTimeData, setReadingTimeData] = useState();
     // Set the total seconds for the countdown
     const [seconds, setSeconds] = useState(0);
     const [totalSeconds, setTotalSeconds] = useState(0);
+    const { user } = useAuthContext();
     const handleChange = (e) => {
         setSeconds(e.target.value);
     }
@@ -18,19 +23,51 @@ const TimeSwap = () => {
         setSeconds(0);
     }
 
+    const checkScreenTimeData = useCallback(async () => {
+        try {
+            const response = await fetch(`${process.env.REACT_APP_LOCAL_HOST}/time-swap/reading-time`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${user.token}`,
+                }
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                if (data.readingTimePerDay.length > 0) {
+                    setReadingTimeData(data.readingTimePerDay);
+                    setHasAlreadyAddedScreenTime(true);
+                }
+            } else {
+                setHasAlreadyAddedScreenTime(false);
+                throw new Error('Error checking screen time data existence');
+            }
+        } catch (error) {
+            setHasAlreadyAddedScreenTime(false);
+            console.error('Error:', error);
+        }
+    }, [user.token]);
+    useEffect(() => {
+
+        checkScreenTimeData();
+    }, [user.token, checkScreenTimeData]);
+
     return (
         <>
             <NavBar />
+            {isOpenAddScreenTime && <AddScreenTimeModal setIsOpen={setIsOpenAddScreenTime} checkScreenTimeData={checkScreenTimeData} />}
             {/* <form onSubmit={handleSubmit}>
-                <input type="number" value={seconds} onChange={handleChange} />
+                <input type="number" value={seconds} onChange={handleChange} />a
                 <Button type="submit" onClick={handleSubmit}>Submit</Button>
             </form> */}
 
             {/* <Countdown seconds={totalSeconds} /> */}
-
-            <Calendar />
-
-            <ReadingTimeTable />
+            {hasAlreadyAddedScreenTime ?
+                <ReadingTimeTable readingTimeData={readingTimeData} />
+                :
+                <Button onClick={() => setIsOpenAddScreenTime(true)}>Add your screen time for the previous week</Button>
+            }
         </>
     );
 };
