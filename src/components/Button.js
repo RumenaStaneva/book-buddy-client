0fdffab8f5fbed067b1cd329851699e4b6eea46c
Rumena Staneva <rumenastaneva2106@gmail.com>
2023-11-11 17:13:37 +0200

    fix dependencies

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = ({ children, ...rest }) => {
+    return (
+        <button {...rest}>
+            {children}
+        </button>
+    );
+};
+
+export default Button;
