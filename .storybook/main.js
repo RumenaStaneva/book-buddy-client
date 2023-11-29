967f8213326a3be4328348e2d5931504ff616287
Rumena Staneva <rumenastaneva2106@gmail.com>
2023-11-29 13:26:50 +0200

    added basic storybook configuration

---
+++ .storybook/main.js
@@ -0,0 +1,31 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {
+      builder: {
+        useSWC: true,
+      },
+    },
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  staticDirs: ["..\\public"],
+  webpackFinal: async (config) => {
+    config.module.rules = config.module.rules.filter(
+      (rule) => !rule.loader || rule.loader.indexOf('react-docgen-loader') === -1
+    );
+
+    return config;
+  },
+};
+export default config;
